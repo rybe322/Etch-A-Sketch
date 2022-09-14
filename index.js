126a70099b318dacab4e1d3c9e14c7fde8003cad
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-09-14 09:28:56 -0500

    Add change grid size, change color, and delete drawing features

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 let numOfEvents = 0; // keep track of mouse events
+let currentGridSize = 16;
+let currentColor = 'black'
+
 
 const sizeButton = document.querySelector('#sizeButton')
 sizeButton.addEventListener('click', handleButtonClick)
+
+const colorChooseButton = document.querySelector('#chooseColorButton')
+console.log(colorChooseButton)
+colorChooseButton.addEventListener('click', handleChooseColor)
+
+const deleteDrawingButton = document.querySelector('#deleteDrawing')
+deleteDrawingButton.addEventListener('click', handleDeleteDrawing)
+
+const gridSizeTextElement = document.querySelector('#gridSize')
+gridSizeTextElement.textContent = `Current Grid: ${currentGridSize} X ${currentGridSize}`
+
+function updater(newSize = 16) {
+  removeAllChildNodes() // Start with a fresh grid
+  currentGridSize = newSize // Update the grid size
+  // Update the Current Grid Size
+  const gridSizeTextElement = document.querySelector('#gridSize')
+  gridSizeTextElement.textContent = `Current Grid: ${currentGridSize} X ${currentGridSize}`
+
+  const currentColorElement = document.querySelector('#currentColorElement')
+  currentColorElement.style.color = currentColor
+  currentColorElement.textContent = `Your current color is: ${currentColor}`
+}
 
 function removeAllChildNodes() {
   const root = document.querySelector('#grid')
@@ -10,6 +35,7 @@ function removeAllChildNodes() {
 
 function createGrid(size=16) {
   removeAllChildNodes()
+  updater(size);
   const grid = document.querySelector('#grid')
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -24,7 +50,7 @@ function createGrid(size=16) {
 }
 
 function handleMouseEnter() {
-  console.log('enter')
+  /*
   numOfEvents += 1
   if (this.style.backgroundColor === 'white'){  
     if (numOfEvents % 10 === 0) {
@@ -39,6 +65,19 @@ function handleMouseEnter() {
   else {
     return;
   }
+  */
+ this.style.backgroundColor = currentColor;
+}
+
+function handleChooseColor() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  colorString = `#${randomColor}`
+  currentColor = colorString
+  console.log(currentColor)
+  const currentColorElement = document.querySelector('#currentColorElement')
+  currentColorElement.style.color = currentColor
+  currentColorElement.textContent = `Your current color is: ${currentColor}`
+
 }
 
 function handleButtonClick() {
@@ -52,5 +91,8 @@ function handleButtonClick() {
   createGrid(newSize)
 }
 
+function handleDeleteDrawing() {
+  createGrid(currentGridSize)
+}
 
 createGrid()
